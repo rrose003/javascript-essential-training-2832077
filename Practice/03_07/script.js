f935d34da804482ea0179d34503a875ea5bfcd3e
rrose003 <rachelsmacair@rachels-air.lan>
2022-06-17 08:42:34 -0600

    Create object with properties in 3_07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,22 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const blenderBottle = {
+    name: "My waterbottle",
+    volume: 26,
+    color: "black",
+    liquid: "water",
+    bottleLiquidAmount: 26,
+    newLiquidAmount: function (liquidAmount){
+        this.bottleLiquidAmount += liquidAmount;
+    },
+    capOn: true,
+    toggleCap: function (capStatus){
+        this.capOn = capStatus;
+    }
+}
+
+console.log("This is the blenderBottle object: ", blenderBottle);
+
+console.log("This is the bottleLiquidAmount: ", blenderBottle.bottleLiquidAmount);
+
